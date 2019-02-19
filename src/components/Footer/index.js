3647aeefc4dfadd14d4116632eb155a2702e6237
Gregory Lagranha <gregory_lagranha@hotmail.com>
2019-02-18 23:33:15 -0300

    initial structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Footer = () => <footer />;
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Header = () => <header />;
+
+export default Header;
